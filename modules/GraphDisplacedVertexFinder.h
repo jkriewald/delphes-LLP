c92ee2ec8ac9e643f1e45d72e919588f1ec0fc13
jkriewald <jonathankriewald@hotmail.de>
2025-10-01 09:09:47 +0200

    Add license to header

--- modules/GraphDisplacedVertexFinder.h
+++ modules/GraphDisplacedVertexFinder.h
@@ -1,3 +1,33 @@
+/*
+ *  Delphes: a framework for fast simulation of a generic collider experiment
+ *  Copyright (C) 2012-2014  Universite catholique de Louvain (UCL), Belgium
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+ /**  \class GraphDisplacedVertexFinder
+ * 
+ * Find displaced vertices via graph-based clustering of tracks/eflow-tracks
+ * Fit the vertex candidates with a coupled Gauss-Newton fit as outlined in arXiv:2510.XXXXXX
+ * Input is a DelphesCollection of tracks (or eflow-tracks) that have a 5x5 covariance matrix (from TrackCovariance or TrackSmearing)
+ * Output is a DelphesCollection of vertices and re-fitted tracks that belong to the DVs
+ * 
+ * If timing information is available (via TimeSmearing), vertex time is fitted and can be used as gates in the graph
+ * 
+ * \author Jonathan Kriewald 
+ */
+ 
 #ifndef GraphDisplacedVertexFinder_h
 #define GraphDisplacedVertexFinder_h
 
